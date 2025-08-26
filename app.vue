3d8589b93f8bb2b8563e97f547cf24282178ddbc
Komgrich Srisak <komgrichsrisak@MacBook-Pro-khxng-Komgrich.local>
2025-08-27 02:58:10 +0700

    set Components's position success

--- app.vue
+++ app.vue
@@ -1,31 +1,53 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <div class="bg-surface-variant pa-4 position-relative rounded-lg ma-2">
-    <div class="bg-surface-light position-relative py-3 ps-3">
-      <div class="overflow-y-auto pe-3" style="max-height: 250px">
-        <div>Relative parent</div>
 
-        <div
-          v-for="n in 5"
-          :key="n"
-        >
-          <div class="bg-primary position-sticky top-0 pa-3 mt-2">
-            Sticky header {{ n }}
+    
+      <div class="bg-surface-variant pa-4 position-relative rounded-lg ma-2">
+        <div class="bg-surface-light position-relative py-3 ps-3">
+          <div class="position-relative pe-3" style="max-height:max-content">
+            <div>
+              <div>
+                <Header_/>
+              </div>
+              Relative parent
+            </div>
+            <div class="position-relative" style="">
+              <div class="bg-info rounded-lg pa-3 mt-2">
+                <div>
+                  <MainForm1/>
+                </div>
+              </div>
+              <div class="bg-info rounded-lg pa-3 mt-2">
+                <div>
+                  <MainForm2/>
+                </div>
+              </div>
+              <div class="bg-info rounded-lg pa-3 mt-2">
+                <div>
+                  <ButtonSubmit/>
+                </div>
+              </div>
+              <!-- <div class="bg-info rounded-lg pa-3 mt-2">
+                Static child
+              </div> -->
+            </div>
+            <!-- <div
+              v-for="n in 5"
+              :key="n"
+            >
+              <div class="bg-primary position-sticky top-0 pa-3 mt-2">
+                Sticky header {{ n }}
+              </div>
+
+              <div class="bg-info rounded-lg pa-3 mt-2">
+                Static child
+              </div>
+
+            </div> -->
           </div>
-
-          <div
-            v-for="k in 8"
-            :key="k"
-            class="bg-info rounded-lg pa-3 mt-2"
-          >
-            Static child
-          </div>
-
         </div>
       </div>
-    </div>
-  </div>
   
     <!-- <div>
       <div>
@@ -50,6 +72,7 @@ import Header_ from "./components/Header.vue"
 import MainForm1 from "./components/MainForm1.vue"
 import MainForm2 from "./components/MainForm2.vue"
 import ButtonSubmit from "./components/ButtonSubmit.vue"
+
 
 
 </script>
